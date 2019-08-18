e50a91c2a75c52d11aca26e5553625bd974fee0f
Sebastian Maleck <s.maleck@outlook.de>
2019-08-18 21:43:21 +0200

    Adds Language Switching

--- Assets/_Source/Features/TitleMenu/HowToPlayView.cs
+++ Assets/_Source/Features/TitleMenu/HowToPlayView.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _Source.Services.Texts;
 using _Source.Util;
+using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace _Source.Features.TitleMenu
 {
-    public class HowToPlayView : AbstractView, IInitializable
+    public class HowToPlayView : AbstractView, IInitializable, ILocalizable
     {
         public class Factory : PlaceholderFactory<UnityEngine.Object, HowToPlayView> { }
 
+        [Header("Close Button")]
+        [SerializeField] private Button _closeButton;
+
         public void Initialize()
+        {
+            
+        }
+
+        public void Localize()
         {
             
         }
--- Assets/_Source/Features/TitleMenu/SettingsView.cs
+++ Assets/_Source/Features/TitleMenu/SettingsView.cs
@@ -1,6 +1,9 @@
-﻿using _Source.Services.Texts;
+﻿using _Source.Features.SceneManagement;
+using _Source.Services.Texts;
 using _Source.Util;
+using System;
 using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -11,19 +14,62 @@ namespace _Source.Features.TitleMenu
     {
         public class Factory : PlaceholderFactory<UnityEngine.Object, SettingsView> { }
 
+        [Header("Close Button")]
+        [SerializeField] private Button _closeButton;
+
         [Header("Language Buttons")]
         [SerializeField] private Button _englishLanguageButton;
         [SerializeField] private TextMeshProUGUI _englishLanguageButtonText;
         [SerializeField] private Button _germanLanguageButton;
         [SerializeField] private TextMeshProUGUI _germanLanguageButtonText;
 
-        [Header("Close Button")]
-        [SerializeField] private Button _closeButton;
-        [SerializeField] private TextMeshProUGUI _closeButtonText;
+        private ISceneManagementController _sceneManagementController;
+
+        [Inject]
+        private void Inject(ISceneManagementController sceneManagementController)
+        {
+            _sceneManagementController = sceneManagementController;
+        }
 
         public void Initialize()
         {
+            _closeButton.OnClickAsObservable()
+                .Subscribe(_ => Close())
+                .AddTo(Disposer);
+
+            _englishLanguageButton.OnClickAsObservable()
+                .Subscribe(_ => SwitchLanguageTo(Language.English))
+                .AddTo(Disposer);
+
+            _germanLanguageButton.OnClickAsObservable()
+                .Subscribe(_ => SwitchLanguageTo(Language.German))
+                .AddTo(Disposer);
+
+            SetupLanguageButtons();
             Localize();
+        }
+
+        private void SetupLanguageButtons()
+        {
+            switch (TextService.CurrentLanguage)
+            {
+                case Language.English:
+                    _englishLanguageButton.interactable = false;
+                    _germanLanguageButton.interactable = true;
+                    break;
+                case Language.German:
+                    _englishLanguageButton.interactable = true;
+                    _germanLanguageButton.interactable = false;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void SwitchLanguageTo(Language language)
+        {
+            TextService.SetLanguage(language);
+            _sceneManagementController.ToTitle();
         }
 
         public void Localize()
