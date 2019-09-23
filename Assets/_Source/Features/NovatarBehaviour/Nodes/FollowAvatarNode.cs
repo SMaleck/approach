48c2103ce72639994515ef50412a74b6f6574b9c
Sebastian Maleck <s.maleck@outlook.de>
2019-09-23 18:16:39 +0200

    Adds RangeSensor

---
+++ Assets/_Source/Features/NovatarBehaviour/Nodes/FollowAvatarNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentBehaviourTree;
+
+namespace _Source.Features.NovatarBehaviour.Nodes
+{
+    public class FollowAvatarNode : IBehaviourTreeNode
+    {
+        public FollowAvatarNode()
+        {
+
+        }
+
+        public BehaviourTreeStatus Tick(TimeData time)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected bool IsInFollowRange()
+        {
+            var isInRange = NovatarStateModel.CurrentDistanceToAvatar.Value <= NovatarEntity.SqrRange;
+            return isInRange && !IsInTouchRange();
+        }
+
+        protected bool IsInTouchRange()
+        {
+            return NovatarStateModel.CurrentDistanceToAvatar.Value <= NovatarEntity.SqrTargetReachedThreshold;
+        }
+    }
+}
---
+++ Assets/_Source/Features/NovatarBehaviour/Sensors/RangeSensor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _Source.Entities;
+using _Source.Entities.Avatar;
+using _Source.Entities.Novatar;
+using Zenject;
+
+namespace _Source.Features.NovatarBehaviour.Sensors
+{
+    public class RangeSensor
+    {
+        public class Factory : PlaceholderFactory<INovatar, RangeSensor> { }
+
+        private readonly INovatar _novatarEntity;
+        private readonly IAvatar _avatarEntity;
+
+        private float _sqrDistanceToAvatar;
+
+        public RangeSensor(
+            INovatar novatarEntity,
+            IAvatar avatarEntity)
+        {
+            _novatarEntity = novatarEntity;
+            _avatarEntity = avatarEntity;
+        }
+
+        public void UpdateSensorReadings()
+        {
+            var distance = _novatarEntity.Position - _avatarEntity.Position;
+            _sqrDistanceToAvatar = distance.sqrMagnitude;
+        }
+
+        public bool IsInFollowRange()
+        {
+            var isInRange = _sqrDistanceToAvatar <= _novatarEntity.SqrRange;
+            return isInRange && !IsInTouchRange();
+        }
+
+        public bool IsInTouchRange()
+        {
+            return _sqrDistanceToAvatar <= _novatarEntity.SqrTargetReachedThreshold;
+        }
+    }
+}
