ffad6ebf88eea1e84e6eb62fada49b3b263d6e26
Sebastian Maleck <s.maleck@outlook.de>
2019-11-07 20:53:34 +0100

    Adds CiBuilder

---
+++ Assets/Editor/CiBuild/CiBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor.CiBuild
+{
+    public static class CiBuilder
+    {
+        public static void Run()
+        {
+            EditorApplication.Exit(0);
+        }
+    }
+}
