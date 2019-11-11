9d3a9039281d41ed29530cfe72cc638b3697817c
Sebastian Maleck <s.maleck@outlook.de>
2019-11-11 13:10:50 +0100

    Adds build script

--- Assets/Editor/CiBuild/CiBuilder.cs
+++ Assets/Editor/CiBuild/CiBuilder.cs
@@ -10,8 +10,33 @@ namespace Assets.Editor.CiBuild
 {
     public static class CiBuilder
     {
+        private static readonly string[] BuildScenes =
+        {
+            "Assets/InitScene.unity", 
+            "Assets/TitleScene.unity" , 
+            "Assets/GameScene.unity"
+        };
+
+        private const string AndroidBuildPath = "androidBuild";
+
         public static void Run()
         {
+            Debug.Log("Starting Android Build...");
+
+            Debug.Log($"Path: {AndroidBuildPath}");
+            Debug.Log("Scenes:");
+            foreach (var scene in BuildScenes)
+            {
+                Debug.Log(scene);
+            }
+
+            BuildPipeline.BuildPlayer(
+                BuildScenes, 
+                AndroidBuildPath, 
+                BuildTarget.Android, 
+                BuildOptions.None);
+            
+            Debug.Log("...Android Build DONE!");
             EditorApplication.Exit(0);
         }
     }
