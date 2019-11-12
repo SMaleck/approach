15d89acef3a3e3e6bca6aae33108bc1bc68df942
Sebastian Maleck <s.maleck@outlook.de>
2019-11-12 13:02:31 +0100

    Cleans up logs

--- Assets/Editor/CiBuild/CiBuilder.cs
+++ Assets/Editor/CiBuild/CiBuilder.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using UnityEditor;
-using UnityEditor.PackageManager;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 namespace Assets.Editor.CiBuild
@@ -14,20 +8,20 @@ namespace Assets.Editor.CiBuild
     {
         private static readonly string[] BuildScenes =
         {
-            "Assets/InitScene.unity", 
-            "Assets/TitleScene.unity" , 
+            "Assets/InitScene.unity",
+            "Assets/TitleScene.unity" ,
             "Assets/GameScene.unity"
         };
 
+        private const string ApkName = "approach.apk";
         private const string AndroidBuildPath = "androidBuild";
-
-        private static AddRequest _installPackagesRequest;
 
         public static void Run()
         {
             var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("\n\n----------- BUILD SCRIPT -----------\n");
             stringBuilder.AppendLine("Starting Android Build...");
-            stringBuilder.AppendLine($"Path: {AndroidBuildPath}");
+            stringBuilder.AppendLine($"APK Path: {ApkName}");
             stringBuilder.AppendLine("Scenes:");
             foreach (var scene in BuildScenes)
             {
@@ -43,11 +37,11 @@ namespace Assets.Editor.CiBuild
         {
             BuildPipeline.BuildPlayer(
                 BuildScenes,
-                AndroidBuildPath,
+                ApkName,
                 BuildTarget.Android,
                 BuildOptions.None);
 
-            Debug.Log("...Android Build DONE!");
+            Debug.Log("\n\n...Android Build DONE!");
         }
     }
 }
