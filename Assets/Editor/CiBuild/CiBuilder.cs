fdfc331821050ecef606580db54f26ef24d3403d
Sebastian Maleck <s.maleck@outlook.de>
2019-11-13 17:38:09 +0100

    Android path

--- Assets/Editor/CiBuild/CiBuilder.cs
+++ Assets/Editor/CiBuild/CiBuilder.cs
@@ -16,7 +16,7 @@ namespace Assets.Editor.CiBuild
         private const string ApkName = "approach.apk";
         private const string AndroidBuildPath = "androidBuild";
 
-        private const string AndroidSdkRoot = "/opt/tools/";
+        private const string AndroidSdkRoot = "/opt/android-sdk/";
         private const string AndroidNdkRoot = "/opt/ndk/";
 
         public static void Run()
