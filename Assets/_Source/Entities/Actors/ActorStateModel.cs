0d2990cc49584e6182473b85cf43c043d67a7518
Sebastian Maleck <sebastian.maleck@kolibrigames.com>
2021-01-05 15:10:05 +0100

    Cleanup

--- Assets/_Source/Entities/Actors/ActorStateModel.cs
+++ Assets/_Source/Entities/Actors/ActorStateModel.cs
@@ -7,11 +7,12 @@ using Zenject;
 
 namespace _Source.Entities.Actors
 {
-    public class ActorStateModel : AbstractDisposableFeature
+    public class ActorStateModel : AbstractDisposableFeature, IActorStateModel
     {
         public class Factory : PlaceholderFactory<ActorStateModel> { }
 
         private readonly Dictionary<Type, IDataComponent> _dataComponents;
+        private readonly List<IResettableDataComponent> _resettableDataComponents;
 
         public IDataComponent this[Type type] => _dataComponents[type];
 
@@ -21,6 +22,8 @@ namespace _Source.Entities.Actors
         public ActorStateModel()
         {
             _dataComponents = new Dictionary<Type, IDataComponent>();
+            _resettableDataComponents = new List<IResettableDataComponent>();
+            _onReset = new Subject<Unit>().AddTo(Disposer);
         }
 
         public ActorStateModel Attach(IDataComponent dataComponent)
@@ -28,11 +31,17 @@ namespace _Source.Entities.Actors
             _dataComponents.Add(dataComponent.GetType(), dataComponent);
             dataComponent.AddTo(Disposer);
 
+            if (dataComponent is IResettableDataComponent resettable)
+            {
+                _resettableDataComponents.Add(resettable);
+            }
+
             return this;
         }
 
         public void Reset()
         {
+            _resettableDataComponents.ForEach(e => e.Reset());
             _onReset.OnNext(Unit.Default);
         }
     }
---
+++ Assets/_Source/Entities/Actors/IActorStateModel.cs
@@ -0,0 +1,14 @@
+﻿using _Source.Entities.Actors.DataComponents;
+using System;
+using UniRx;
+
+namespace _Source.Entities.Actors
+{
+    public interface IActorStateModel
+    {
+        IDataComponent this[Type type] { get; }
+        IOptimizedObservable<Unit> OnReset { get; }
+        ActorStateModel Attach(IDataComponent dataComponent);
+        void Reset();
+    }
+}
