e4a8cea71c48462c52196f56bf76b39a322511b3
Sebastian Maleck <s.maleck@outlook.com>
2021-03-22 20:40:57 +0100

    Adds DebugConstants

---
+++ Assets/_Source/Debug/DebugConstants.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Source.Debug
+{
+    public static class DebugConstants
+    {
+        public static readonly Vector3 IdLabelOffset = new Vector3(0, 1.1f, 0);
+        public static readonly Vector3 SensorVisualLabelOffset = new Vector3(0, 1.7f, 0);
+        public static readonly Vector3 SensorTouchLabelOffset = new Vector3(0, 2.3f, 0);
+    }
+}
