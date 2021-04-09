8b94b1ed95f81c771045288373e74b4f2512a452
Sebastian Maleck <s.maleck@outlook.com>
2021-04-09 16:42:58 +0200

    Adds reactions

--- Assets/_Source/Features/ActorEntities/Components/StateReactionComponent.cs
+++ Assets/_Source/Features/ActorEntities/Components/StateReactionComponent.cs
@@ -2,19 +2,20 @@
 using _Source.Features.ActorEntities.Config;
 using _Source.Features.Actors.DataComponents;
 using DG.Tweening;
+using Packages.RxUtils;
 using UniRx;
 using UnityEngine;
 
 namespace _Source.Features.ActorEntities.Components
 {
-    // ToDo V1 ENEMY: Shake
-    // ToDo V1 FRIEND: Bump Scale
     public class StateReactionComponent : AbstractMonoComponent
     {
-        [SerializeField] private Transform _scaleParent;
+        [SerializeField] private Transform _reactionParent;
+        [SerializeField] private StateReactionConfig _config;
 
         private RelationshipDataComponent _relationshipDataComponent;
-        private SerialDisposable _tweenDisposer;
+        private CompositeDisposable _friendTweenDisposer;
+        private CompositeDisposable _enemyTweenDisposer;
 
         protected override void OnSetup()
         {
@@ -23,7 +24,8 @@ namespace _Source.Features.ActorEntities.Components
 
         protected override void OnStart()
         {
-            _tweenDisposer = new SerialDisposable().AddTo(Disposer);
+            _friendTweenDisposer = new CompositeDisposable().AddTo(Disposer);
+            _enemyTweenDisposer = new CompositeDisposable().AddTo(Disposer);
 
             _relationshipDataComponent.Relationship
                 .Subscribe(OnRelationshipSwitched)
@@ -32,22 +34,46 @@ namespace _Source.Features.ActorEntities.Components
 
         protected override void OnStop()
         {
-            _tweenDisposer?.Dispose();
+            _friendTweenDisposer?.Dispose();
+            _enemyTweenDisposer?.Dispose();
+
+            _reactionParent.localScale = Vector3.one;
         }
 
         private void OnRelationshipSwitched(EntityState state)
         {
+            switch (state)
+            {
+                case EntityState.Enemy:
+                    ToEnemy();
+                    break;
 
+                case EntityState.Friend:
+                    ToFriend();
+                    break;
+
+                default:
+                    break;
+            }
         }
 
         private void ToEnemy()
         {
-
+            _reactionParent.DOShakePosition(
+                    _config.EnemyShakeSeconds,
+                    _config.EnemyShakeStrength,
+                    _config.EnemyShakeVibration)
+                .AddTo(_enemyTweenDisposer);
         }
 
         private void ToFriend()
         {
-
+            _reactionParent.DOScale(
+                    _config.FriendGrowScale,
+                    _config.FriendGrowSeconds)
+                .SetLoops(2, LoopType.Yoyo)
+                .SetEase(Ease.InOutCubic)
+                .AddTo(_friendTweenDisposer);
         }
     }
 }
---
+++ Assets/_Source/Features/ActorEntities/Config/StateReactionConfig.cs
@@ -0,0 +1,26 @@
+﻿using _Source.App;
+using UnityEngine;
+
+namespace _Source.Features.ActorEntities.Config
+{
+    [CreateAssetMenu(fileName = nameof(StateReactionConfig), menuName = Constants.ConfigMenu + nameof(StateReactionConfig))]
+    public class StateReactionConfig : ScriptableObject
+    {
+        [Header("Friend")]
+        [SerializeField] private float _friendGrowScale;
+        public float FriendGrowScale => _friendGrowScale;
+
+        [SerializeField] private float _friendGrowSeconds;
+        public float FriendGrowSeconds => _friendGrowSeconds;
+
+        [Header("Enemy")]
+        [SerializeField] private float _enemyShakeStrength;
+        public float EnemyShakeStrength => _enemyShakeStrength;
+
+        [SerializeField] private float _enemyShakeSeconds;
+        public float EnemyShakeSeconds => _enemyShakeSeconds;
+
+        [SerializeField] private int _enemyShakeVibration;
+        public int EnemyShakeVibration => _enemyShakeVibration;
+    }
+}
